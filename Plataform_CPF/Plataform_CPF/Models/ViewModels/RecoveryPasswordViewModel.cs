516daa677d057b23e919ace1a2a72c1fea71a330
GOSJ GOMSO <GOMSO_07@outlook.com>
2021-08-25 16:37:11 -0500

    MOD1--LOGIN Y REGISTROS: LOGIN Y RECUPERACONTRA

---
+++ Plataform_CPF/Plataform_CPF/Models/ViewModels/RecoveryPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Plataform_CPF.Models.ViewModels
+{
+    public class RecoveryPasswordViewModel
+    {
+        public string token { get; set; }
+        [Required(ErrorMessage = "EL CAMPO ES REQUERIDO")]
+        public string Password { get; set; }
+        [Compare("Password")]
+        [Required(ErrorMessage = "EL CAMPO ES REQUERIDO")]
+        public string Password2 { get; set; }
+    }
+}
---
+++ Plataform_CPF/Plataform_CPF/Models/ViewModels/RecoveryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Plataform_CPF.Models.ViewModels
+{
+    public class RecoveryViewModel
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "EL CAMPO ES REQUERIDO")]
+        public string Email { get; set; }
+    }
+}
