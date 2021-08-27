55b9ac9b582660d6d0e5d9a60e60919be0d9bc9e
GOSJ GOMSO <GOMSO_07@outlook.com>
2021-08-27 13:23:36 -0500

    MOD4--MAESTROS:INTERFACES INICIALES

---
+++ Plataform_CPF/Plataform_CPF/Controllers/MaestrosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using Plataform_CPF.Models;
+
+using System.Globalization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Plataform_CPF.Models.ViewModels;
+using Plataform_CPF.Repositories;
+using System.Data.Entity.Validation;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Plataform_CPF.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private BDCPFORIEntities db = new BDCPFORIEntities();
+        private IUsuarioRepository _repoUsuario;
+
+        public TeacherController(IUsuarioRepository repository)
+        {
+            _repoUsuario = repository;
+        }
+
+        public TeacherController() : this(new UsuarioRepository())
+        {
+
+        }
+
+        public ActionResult _LayoutMT(int? id)
+        {
+            if (Session["UserM"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult HomeT(int? mesg, int? id)
+        {
+            return View();
+        }
+        // GET: Alumnos        
+        public ActionResult Index()
+        {
+            var usuario = db.Usuarios.Include(u => u.perfil);
+            return View(usuario.ToList());
+        }
+
+        public ActionResult TPerfil(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios u = db.Usuarios.Find(id);
+            if (u == null)
+            {
+                return HttpNotFound();
+            }
+            return View(u);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TPerfil([Bind(Include = "idUsuario,usuario,correo,contraseña,perfil,status,TokenRecovery")] Usuarios u)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(u).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("HomeT");
+            }
+            return View(u);
+        }
+
+
+        public ActionResult MConfiguracion(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maestros m = db.Maestros.Find(id);
+            if (m == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idUsuario = new SelectList(db.Usuarios, "idUsuario", "usuario", m.idUsuario);
+            return View(m);
+        }
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MConfiguracion([Bind(Include = "idMaestro,nombre,app,apm,sexo,direccion,telefono,seccion,idUsuario")] Maestros m)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(m).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idUsuario = new SelectList(db.Usuarios, "idUsuario", "usuario", m.idUsuario);
+            return View(m);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["idUs"] = null;
+            Session["nombre"] = null;
+            Session["UserM"] = null;
+            ViewBag.M = "USTED HA SALIDO DE SU SESIÓN";
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
